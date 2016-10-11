5ad36578dbfdbd57b65d3f2156ec57b25140ada4
Daniel <truenorthtrainingcompany@gmail.com>
2016-10-11 16:10:34 -0400

    Validates Contact fields Tests now pass rspec spec/models/contact_spec.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,3 @@
 class Contact < ApplicationRecord
+  validates_presence_of :full_name, :email, :phone_number, :address
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  it "has a valid factory" do
-    expect(contact).to be_valid
+ describe Contact do
+    it { is_expected.to validate_presence_of(:full_name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:phone_number) }
+    it { is_expected.to validate_presence_of(:address) }
   end
 end
