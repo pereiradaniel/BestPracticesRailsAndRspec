885cd2b7ac214c2d3979822352024a7e86f69357
Daniel <truenorthtrainingcompany@gmail.com>
2016-10-11 16:03:36 -0400

    Migration reset Rspec is now running

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011194918) do
+ActiveRecord::Schema.define(version: 20161011200221) do
 
+  create_table "contacts", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "email"
+    t.integer  "phone_number"
+    t.text     "address"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
