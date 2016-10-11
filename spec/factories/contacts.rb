785eba3af90067352eb002dd3a39dc141613a5f7
Daniel <truenorthtrainingcompany@gmail.com>
2016-10-11 15:39:23 -0400

    Create a Factory Create directory named 'factories' under 'spec' folder

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,9 @@
+# Create Fixtures using Factory Girl and Faker
+FactoryGirl.define do
+  factory :contact do
+    full_name     { Faker::Name.name }
+    email         { Faker::Internet.email }
+    phone_number  { Faker::PhoneNumber.phone_number }
+    address       { Faker::Address.street_address }
+  end
+end
